e5c0c1e70a6b8abf984468c62aa7a1e4d16ebeaf
Vishal_Bait <vishalbait01@gmail.com>
2020-08-22 12:24:06 +0530

    Add user model
    1. User model to store user information in mongo database

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
-const conn = mongoose.connect("mongodb://localhost:27017/instagram");
+const conn = mongoose.connect("mongodb://localhost:27017/instagram", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Database is connected");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listen on port ${PORT}`);
