ecdb63c9bc963768eec3d6cdb16625b982b73802
Vishal_Bait <vishalbait01@gmail.com>
2020-09-03 16:04:59 +0530

    Add Axios code to save register and to verify login 1. Delete yarn.lock to avoid issue with package-lock.json

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 import styles from "../scss/Form.module.scss";
+import axios from "axios";
 
 const Login = () => {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const postLogin = () => {
+    axios({
+      url: "/login",
+      method: "post",
+      data: {
+        username: data.username,
+        password: data.password,
+      },
+    })
+      .then((data) => {
+        console.log(data.data.token);
+        console.log(data);
+        alert("You are Successfully Login");
+        document.getElementById("loginForm").reset();
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
   return (
     <Container>
       <Row className={styles.login}>
@@ -10,18 +36,28 @@ const Login = () => {
           <Card.Header className="my-3">
             <h3 className="text-uppercase font-time">Login</h3>
           </Card.Header>
-          <Form>
+          <Form id="loginForm">
             <Form.Group>
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter Your Username" />
+              <Form.Control
+                type="text"
+                onChange={(e) => setData({ ...data, username: e.target.value })}
+                placeholder="Enter Your Username"
+              />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter Your Password" />
+              <Form.Control
+                type="password"
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+                placeholder="Enter Your Password"
+              />
             </Form.Group>
 
-            <Button variant="primary">Login</Button>
+            <Button onClick={() => postLogin()} variant="primary">
+              Login
+            </Button>
           </Form>
         </Col>
       </Row>
--- Client/src/components/Register.js
+++ Client/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Card,
@@ -9,8 +9,38 @@ import {
   Alert,
 } from "react-bootstrap";
 import styles from "../scss/Form.module.scss";
+import axios from "axios";
 
 const Register = () => {
+  const [data, setData] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    conPassword: "",
+  });
+
+  const postRegister = () => {
+    axios({
+      url: "/signup",
+      method: "post",
+      data: {
+        name: data.name,
+        username: data.username,
+        password: data.password,
+        email: data.email,
+      },
+    })
+      .then((data) => {
+        console.log(data);
+        alert("Successful");
+        document.getElementById("registerForm").reset();
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
   return (
     <Container>
       <Row className={styles.login}>
@@ -19,53 +49,62 @@ const Register = () => {
             <h3 className="text-uppercase font-time">Register</h3>
           </Card.Header>
 
-          <Form>
+          <Form id="registerForm">
             <Form.Group>
-              {/* <Form.Label>Username</Form.Label> */}
               <Form.Control
                 type="text"
                 className="rounded-pill"
                 placeholder="Enter Your Username"
+                onChange={(e) => setData({ ...data, username: e.target.value })}
               />
             </Form.Group>
 
             <Form.Group>
-              {/* <Form.Label>Email</Form.Label> */}
               <Form.Control
                 type="email"
                 className="rounded-pill"
                 placeholder="Enter Your Email"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
               />
             </Form.Group>
 
             <Form.Group>
-              {/* <Form.Label>Name</Form.Label> */}
               <Form.Control
                 type="text"
                 className="rounded-pill"
                 placeholder="Enter Your Name"
+                name="name"
+                onChange={(e) => setData({ ...data, name: e.target.value })}
               />
             </Form.Group>
 
             <Form.Group>
-              {/* <Form.Label>Password</Form.Label> */}
               <Form.Control
                 type="password"
                 className="rounded-pill"
                 placeholder="Enter Your Password"
+                name="password"
+                onChange={(e) => setData({ ...data, password: e.target.value })}
               />
             </Form.Group>
 
             <Form.Group>
-              {/* <Form.Label>Confirm Password</Form.Label> */}
               <Form.Control
                 type="password"
                 className="rounded-pill"
                 placeholder="Confirm Your Password"
+                name="conPassword"
+                onChange={(e) =>
+                  setData({ ...data, conPassword: e.target.value })
+                }
               />
             </Form.Group>
 
-            <Button variant="primary" className="mx-auto">
+            <Button
+              variant="primary"
+              className="mx-auto"
+              onClick={() => postRegister()}
+            >
               Register
             </Button>
           </Form>
