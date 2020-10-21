d7220ea2b43808793e0ce2e8c614e80ffa60b719
Vishal_Bait <vishalbait01@gmail.com>
2020-10-21 13:38:58 +0530

    Add dynamic nav link 1. We can see different navigation link depend upon user logged in or not

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 const NavBar = () => {
+  const { state, dispatch } = useContext(UserContext);
+
+  const renderList = () => {
+    if (state) {
+      return [
+        <Link className="font-nav mx-3 text-dark" to="/CreatePost">
+          Create Post
+        </Link>,
+
+        <Link className="font-nav mx-3 text-dark" to="/Profile">
+          Profile
+        </Link>,
+      ];
+    } else {
+      return [
+        <Link className="font-nav mx-3 text-dark" to="/Login">
+          Log In
+        </Link>,
+
+        <Link className="font-nav  mx-3 text-dark" to="/Register">
+          Sign Up
+        </Link>,
+      ];
+    }
+  };
+
   return (
     <Navbar bg="white" className="py-2 shadow" expand="lg">
       <Navbar.Brand>
-        <Link className="font-hotel" to="/">
+        <Link className="font-hotel" to={state ? "/" : "/Login"}>
           Instagram
         </Link>
       </Navbar.Brand>
@@ -14,23 +41,8 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto font-nav">
-          <Link className="text-uppercase  mx-3 text-dark" to="/CreatePost">
-            Create Post
-          </Link>
-
-          <Link className="text-uppercase mx-3 text-dark" to="/Login">
-            Log In
-          </Link>
-
-          <Link className="text-uppercase  mx-3 text-dark" to="/Register">
-            Sign Up
-          </Link>
-
-          <Link className="text-uppercase  mx-3 text-dark" to="/Profile">
-            Profile
-          </Link>
-        </Nav>
+        <Nav className="ml-auto"></Nav>
+        {renderList()}
       </Navbar.Collapse>
     </Navbar>
   );
